fd6c7ae279fc5aea4bc245a15e2a11024c43b729
Andy Chen <andy.g.chen@berkeley.edu>
2022-04-05 15:53:53 -0700

    React Intro Barebones Complete

--- Static React/counter-app/src/App.js
+++ Static React/counter-app/src/App.js
@@ -1,33 +1,36 @@
 import Button from './components/Button';
 import React from 'react';
-import ReactDOM from 'react';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {count: 0};
   }
-
-  incrementCount() {
-    this.state.count += 1;
+  incrementCount = () => {
+    this.setState(
+      {count: this.state.count + 1}
+      );
   }
 
-  decrementCount() {
-    this.state.count += 1;
+  decrementCount = () => {
+    this.setState(
+      {count: this.state.count - 1}
+      );
   }
 
   render() {
     return (
-      <div className="container">
-        <p>Count: <h2>{this.state.count}</h2></p> 
+      <div className="App">
+        <head>
+          <title>React Intro</title>
+        </head>
+        <p>Count: </p> 
+        <h2>{this.state.count}</h2>
         <Button title={"+"} task={this.incrementCount}/>
         <Button title={"-"} task={this.decrementCount}/>
       </div>
     );
   }
 }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
 
 export default App;
